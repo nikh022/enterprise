b9be49b2e2c0b8904567332a31c9b06c86bf40c4
Nitin2332 <nitinrajpoot2332@gmail.com>
2024-11-09 17:33:33 +0530

    farmer photo add

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import Home from "./pages/home/home";
 import Footer from "./components/footer";
 
 function App() {
-  return <div className="bg-gray-800">
-      <Navbar/>
-      <Home/>
-      <Footer/>
-  </div>;
+  return (
+    <>
+      <div className="bg-gray-800">
+        <Navbar />
+        <Home />
+      </div>
+      <div className="bg-gray-950">
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,15 @@
 export default function Home() {
     return (
-        <div className="mx-7"> {/* Adds vertical space between elements */}
+        <div className="flex mx-7"> {/* Adds vertical space between elements */}
             <div className="w-1/2">
-                <h2 className="text-4xl pt-9 pb-9 font-bold text-white leading-relaxed">
+                <h2 className="text-4xl pt-9 pb-10 font-bold text-white leading-relaxed">
                     वर्षा एंटरप्राइज में हम किसानों से गेंहू, चना, मटर, और अन्य अनाज खरीदते हैं।
                     हमारा उद्देश्य है किसानों को उचित मूल्य और भरोसेमंद सेवा प्रदान करना।
                 </h2>
+
+            </div>
+            <div className="mx-20">
+            <img className="h-auto max-w-full " height={500} width={500} src="src/assets/farming.svg" alt="image description"/>
 
             </div>
         </div>
