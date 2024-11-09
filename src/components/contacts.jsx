8d13fac6381fcdb0ea7d70b062e42651292ef643
nikhil022 <niksocial2@gmail.com>
2024-11-09 17:53:16 +0530

    add mobile number

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -5,10 +5,10 @@ export default function Contacts() {
             Contact Us
         </h1>
         <div className="text-emerald-300 pb-4">
-            Mobile no. -
+            Mobile no. - 9984844002, 9005490418
         </div>
         <div className="text-emerald-300">
-            Email Id -
+            Email Id - devendrarajpoot008@gmail.com
         </div>
 
     </div>
